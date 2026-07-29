d7e80facc553763fd8126b30f2f407dff20a08a5
shu <tyaro.rona@gmail.com>
2026-07-29 18:45:39 +0900

    7/29

---
+++ schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS answers (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    age TEXT,
+    gender TEXT,
+    experience TEXT,
+    frequency TEXT,
+    perception TEXT,
+    perception_other TEXT,
+    abroad TEXT,
+    ir_use TEXT,
+    ir_support TEXT,
+    participate TEXT,
+    reasons TEXT,
+    comment TEXT
+);
