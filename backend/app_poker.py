1fb6a3100ab34a36deca267c3051bc75120dc039
shu <tyaro.rona@gmail.com>
2026-07-31 14:35:59 +0900

    aa

--- backend/app_poker.py
+++ backend/app_poker.py
@@ -1,11 +1,17 @@
 from flask import Flask, request, redirect, render_template_string, session
 import psycopg2
 import os
+import socket
 
-DATABASE_URL ="postgresql://postgres:tyaro.0611.2214@db.ncgpjxxrdccsbhwbscjq.supabase.co:5432/postgres"
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def get_conn():
-    return psycopg2.connect(DATABASE_URL, sslmode='require')
+    # IPv4アドレスを強制的に使う
+    host = "db.ncgpjxxrdccsbhwbscjq.supabase.co"
+    ipv4 = socket.gethostbyname(host)
+    dsn = DATABASE_URL.replace(host, ipv4)
+    return psycopg2.connect(dsn, sslmode='require')
+
 from collections import Counter
 
 app = Flask(__name__)
